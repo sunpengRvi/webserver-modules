8da9da2697c382c5008bc166ce0c15859d4dab1f
Peng <psunjohnny@gmail.com>
2023-10-10 06:40:52 +0000

    init

---
+++ services/webserver-cluster/main.tf
@@ -0,0 +1,217 @@
+locals {
+  any_port = 0
+  http_port = 80
+  tcp_protocal = "tcp"
+  all_ips = [ "0.0.0.0/0" ]
+}
+
+data "aws_vpc" "default" {
+  default = true
+}
+
+data "aws_subnets" "default" {
+  filter {
+    name = "vpc-id"
+    values = [ data.aws_vpc.default.id ]
+  }
+}
+
+data "terraform_remote_state" "db" {
+  backend = "s3"
+  
+  config = {
+    bucket = var.db_remote_state_bucket
+    key = var.db_remote_state_key
+    region = "us-east-2"
+  }
+}
+
+#resource "aws_instance" "example" {
+#  ami =  "ami-0fb653ca2d3203ac1"
+#  instance_type = "t2.micro"
+#  vpc_security_group_ids = [ aws_security_group.instance.id ]
+#
+#  user_data = <<-EOF
+#              #!/bin/bash
+#              echo "Hello, World" > index.html
+#              nohup busybox httpd -f -p ${var.server_port} &
+#              EOF
+#
+#  user_data_replace_on_change = true
+#
+#  tags = {
+#    Name = "terraform-example"
+#  }
+#}
+
+resource "aws_launch_configuration" "example" {
+  image_id =  "ami-0fb653ca2d3203ac1"
+  instance_type = var.instance_type
+  security_groups = [ aws_security_group.instance.id ]
+
+  #user_data = <<-EOF
+  #            #!/bin/bash
+  #            echo "Hello, World" > index.html
+  #            echo "${data.terraform_remote_state.db.outputs.address} >> index.html"
+  #            echo "${data.terraform_remote_state.db.outputs.port} >> index.html"
+  #            nohup busybox httpd -f -p ${var.server_port} &
+  #            EOF
+  
+  user_data = templatefile("${path.module}/user-data.sh", {
+    server_port = var.server_port
+    db_address = data.terraform_remote_state.db.outputs.address
+    db_port = data.terraform_remote_state.db.outputs.port
+  })
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+resource "aws_autoscaling_group" "example" {
+  launch_configuration = aws_launch_configuration.example.name
+  vpc_zone_identifier = data.aws_subnets.default.ids
+
+  target_group_arns = [aws_lb_target_group.asg.arn]
+  health_check_type = "ELB"
+  
+  min_size = var.min_size
+  max_size = var.max_size
+
+  tag {
+    key = "Name"
+    value = var.cluster_name
+    propagate_at_launch = true
+  }
+}
+
+resource "aws_security_group" "instance" {
+  name = "${var.cluster_name}-instance"
+
+  ingress {
+    from_port = var.server_port
+    to_port = var.server_port
+    protocol = local.tcp_protocal
+    cidr_blocks = local.all_ips
+  }
+
+}
+
+resource "aws_lb" "example" {
+  name = "${var.cluster_name}-example"
+  load_balancer_type = "application"
+  # Note, this use all subnets. AWS load balances don't consist of a single server, but of multiple servers that run in separate subnets
+  # AWS automatically scales the number of load balancer servers up and down based on traffic and handles failover if one of thoes servers
+  # goes down, so you get scalability and high availability out of the box.
+  subnets = data.aws_subnets.default.ids
+  security_groups = [aws_security_group.alb.id]
+}
+
+resource "aws_lb_listener" "http" {
+  load_balancer_arn = aws_lb.example.arn
+  port = local.http_port
+  protocol = "HTTP"
+
+  default_action {
+    type = "fixed-response"
+
+    fixed_response {
+      content_type = "text/plain"
+      message_body = "404: page not found"
+      status_code = 404
+    }
+  }
+}
+
+resource "aws_security_group" "alb" {
+  name = "${var.cluster_name}-alb"
+
+  #ingress = [
+  #  {
+  #    description = "alb sg"
+  #    from_port = local.http_port
+  #    to_port = local.http_port
+  #    protocol = local.tcp_protocal
+  #    cidr_blocks = local.all_ips
+  #    ipv6_cidr_blocks = []
+  #    prefix_list_ids = []
+  #    security_groups = []
+  #    self = false
+  #  }
+  #]
+  #
+  #egress = [
+  #  {
+  #    description = "alb sg"
+  #    from_port = local.any_port
+  #    to_port = local.any_port
+  #    protocol = -1
+  #    cidr_blocks = local.all_ips
+  #    ipv6_cidr_blocks = []
+  #    prefix_list_ids = []
+  #    security_groups = []
+  #    self = false
+  #  }
+  #]
+}
+
+resource "aws_security_group_rule" "allow_http_inbound" {
+  type = "ingress"
+  security_group_id = aws_security_group.alb.id
+
+  description = "alb sg"
+  from_port = local.http_port
+  to_port = local.http_port
+  protocol = local.tcp_protocal
+  cidr_blocks = local.all_ips
+  ipv6_cidr_blocks = []
+  prefix_list_ids = []
+  self = false
+}
+
+resource "aws_security_group_rule" "allow_all_outbound" {
+  type = "egress"
+  security_group_id = aws_security_group.alb.id
+
+  description = "alb sg"
+  from_port = local.any_port
+  to_port = local.any_port
+  protocol = -1
+  cidr_blocks = local.all_ips
+  ipv6_cidr_blocks = []
+  prefix_list_ids = []
+  self = false
+}
+
+resource "aws_lb_target_group" "asg" {
+  name = "${var.cluster_name}-example"
+  port = var.server_port
+  protocol = "HTTP"
+  vpc_id = data.aws_vpc.default.id
+
+  health_check {
+    path = "/"
+    protocol = "HTTP"
+    matcher = "200"
+    interval = 15
+    timeout = 3
+    healthy_threshold = 2
+    unhealthy_threshold = 2
+  }
+}
+
+resource "aws_lb_listener_rule" "asg" {
+  listener_arn = aws_lb_listener.http.arn
+  priority = 100
+
+  condition {
+    path_pattern {
+      values = [ "*" ]
+    }
+  }
+
+  action {
+    type = "forward"
+    target_group_arn = aws_lb_target_group.asg.arn
+  }
+}
