30c98bd8e89180a4e22a2b30675d716e610594ab
Peng <psunjohnny@gmail.com>
2023-10-12 02:01:27 +0000

    if-else example

--- landing-zone/iam-user/outputs.tf
+++ landing-zone/iam-user/outputs.tf
@@ -2,3 +2,8 @@ output "user_arn" {
   value = aws_iam_user.example.arn
   description = "The ARN of created user"
 }
+
+output "user_name" {
+  value = aws_iam_user.example.name
+  description = "The name of created user"
+}
--- services/webserver-cluster/main.tf
+++ services/webserver-cluster/main.tf
@@ -95,6 +95,26 @@ resource "aws_autoscaling_group" "example" {
   }
 }
 
+resource "aws_autoscaling_schedule" "scale_out_during_business_hours" {
+  count = var.enable_autoscaling ? 1 : 0
+  scheduled_action_name = "scale-out-during-business-hours"
+  min_size = 2
+  max_size = 10
+  desired_capacity = 10
+  recurrence = "0 9 * * *"
+  autoscaling_group_name = aws_autoscaling_group.example.name
+}
+
+resource "aws_autoscaling_schedule" "scale_in_at_night" {
+  count = var.enable_autoscaling ? 1 : 0
+  scheduled_action_name = "scale-in-at-night"
+  min_size = 2
+  max_size = 10
+  desired_capacity = 2
+  recurrence = "0 17 * * *"
+  autoscaling_group_name = aws_autoscaling_group.example.name
+}
+
 resource "aws_security_group" "instance" {
   name = "${var.cluster_name}-instance"
 
